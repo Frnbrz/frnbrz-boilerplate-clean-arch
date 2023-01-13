8fd497481015554871b5eb87f5a6ae19cf10a324
Frnbrz <frnbrz@gmail.com>
2023-01-13 22:45:45 +0100

    Used state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { UserEmptyState } from './moodels'
 import { Home } from './pages'
+import store, { AppStore } from './redux/store'
 
 function App() {
-    return <Home />
+    const user = useSelector((state: AppStore) => state.user)
+
+    return (
+        <>
+            <span>{JSON.stringify(user)}</span>
+            <Home />
+        </>
+    )
 }
 
 export default App
